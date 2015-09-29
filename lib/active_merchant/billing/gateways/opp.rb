7bc4f384a9bf016ff1e2dc7859d52f1bdf0d011d
jake <jake@vertaxe.com>
2015-09-29 10:14:26 +0200

    add logging

--- lib/active_merchant/billing/gateways/opp.rb
+++ lib/active_merchant/billing/gateways/opp.rb
@@ -76,6 +76,8 @@ module ActiveMerchant #:nodoc:
         add_cart_items(post, options)
         add_customer_data(post, options)
         add_options(post, options)
+puts post
+puts options
         commit(post, nil, options)
       end
 
